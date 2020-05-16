b61562403d8b36850ca0588fe0e1c07074aaf650
mostafa <48662854+z0xyz@users.noreply.github.com>
2020-05-16 13:28:35 +0200

    Add files via upload

---
+++ Timer.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python 
+import os
+import time
+import tk_tools
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox
+
+root = tk.Tk()
+root.title("message")
+root.withdraw()
+
+m = 0
+s = 0
+
+def continuation():
+    x = messagebox.askquestion("timer","Do you wanna continue or log-off ?")
+    if x == "yes" : 
+        pass  
+    else : 
+        exit()
+
+while s<=1200:
+    time.sleep(1)
+    s+=1
+    if s==1200:
+        continuation()
+        m+=1
+        s=0
+    
